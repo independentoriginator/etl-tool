c810d5fe2a5a85d11bb91489a97dc2223a4d083c
rsafiullin <rsafiullin@host1>
2022-02-15 16:50:14 +0500

    Unquote while normalizing text

--- etl-repository/src/main/functions/f_normalize_text.sql
+++ etl-repository/src/main/functions/f_normalize_text.sql
@@ -7,5 +7,26 @@ immutable
 parallel safe
 as $function$
 select 
-	nullif(nullif(btrim(regexp_replace(regexp_replace(i_text, '[\n\r]|\s{2,}', ' ', 'g' ), '[^[:print:]]', '', 'g')), '-'), '');
+	nullif(
+		nullif(
+			btrim(
+				btrim(
+					regexp_replace(
+						regexp_replace(
+							i_text
+							, '[\n\r]|\s{2,}'
+							, ' '
+							, 'g' 
+						)
+						, '[^[:print:]]'
+						, ''
+						, 'g'
+					)
+					, '"'
+				)
+			)
+			, '-'
+		)
+		, ''
+	)
 $function$;		
