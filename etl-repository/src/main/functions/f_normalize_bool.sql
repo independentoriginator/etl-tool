bfc142c59a4bac24b2ce8994014156a72e9a9f17
rsafiullin <rsafiullin@host1>
2022-02-15 23:47:50 +0500

    Using the same function parameter name within similar function set

--- etl-repository/src/main/functions/f_normalize_bool.sql
+++ etl-repository/src/main/functions/f_normalize_bool.sql
@@ -1,5 +1,5 @@
 create or replace function f_normalize_bool(
-	i_text text
+	i_raw_value text
 )
 returns boolean
 language sql
@@ -8,8 +8,8 @@ parallel safe
 as $function$
 select 
 	case 
-		when i_text ~* '^[\''\"\s]*1|true|t|yes|y|on|да[\''\"\s]*$' then true 
-		when i_text ~* '^[\''\"\s]*0|false|f|no|n|off|нет[\''\"\s]*$' then false
+		when i_raw_value ~* '^[\''\"\s]*1|true|t|yes|y|on|да[\''\"\s]*$' then true 
+		when i_raw_value ~* '^[\''\"\s]*0|false|f|no|n|off|нет[\''\"\s]*$' then false
 		else null::boolean 
 	end
 $function$;		
