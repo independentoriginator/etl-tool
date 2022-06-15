c50bfe728c866161fc3803ce598f99d2a083d55f
rsafiullin <independentoriginator@gmail.com>
2022-06-15 13:03:45 +0500

    If extension isn't installed

--- etl-repository/src/main/procedures/p_build_target_pgpro_scheduler_job.sql
+++ etl-repository/src/main/procedures/p_build_target_pgpro_scheduler_job.sql
@@ -1,13 +1,41 @@
+do $q$
+begin
+execute format($proc$
 create or replace procedure p_build_target_pgpro_scheduler_job(
 	i_job_rec record
 )
 language plpgsql
 as $procedure$
 declare 
-	l_job_id integer := i_job_rec.target_id;
+	l_pgpro_job_rec record;
 begin
-	if i_job_rec.is_job_exists = false then
-		l_job_id := 
+	%s
+end
+$procedure$;			
+$proc$
+, case 
+	when exists (
+		select 
+			1
+		from 
+			pg_catalog.pg_extension e
+		where 
+			e.extname = 'pgpro_scheduler'
+	) then 
+	$proc_body$
+	select 
+		t.id
+		, t.name
+		, t.comments as description
+		, case when t.active then false else true end as is_disabled
+	into 
+		l_pgpro_job_rec  
+	from 
+		schedule.get_owned_cron() t
+	;
+
+	if l_pgpro_job_rec.id is null then
+		l_pgpro_job_rec.id := 
 			schedule.create_job(
 				jsonb_build_object(
 					'name', i_job_rec.job_name
@@ -17,26 +45,34 @@ begin
 				)
 			);
 	else
-		if nullif(i_job_rec.job_description, i_job_rec.target_description) is not null then
+		if nullif(i_job_rec.job_description, l_pgpro_job_rec.description) is not null then
 			perform 
 				schedule.set_job_attribute(
-					jobid => l_job_id
+					jobid => l_pgpro_job_rec.id
 					, name => 'comments'::text
 					, value => i_job_rec.job_description
 				);
 		end if;	
 	end if;
 			
-	if i_job_rec.is_disabled = true and coalesce(i_job_rec.is_disabled, false) = false then
+	if i_job_rec.is_disabled = true and coalesce(l_pgpro_job_rec.is_disabled, false) = false then
 		perform 
 			schedule.deactivate_job(
-				job_id => l_job_id
+				job_id => l_pgpro_job_rec.id
 			);
-	elsif i_job_rec.is_disabled = false and coalesce(i_job_rec.is_disabled, false) = true then
+	elsif i_job_rec.is_disabled = false and coalesce(l_pgpro_job_rec.is_disabled, false) = true then
 		perform 
 			schedule.activate_job(
-				job_id => l_job_id
+				job_id => l_pgpro_job_rec.id
 			);
 	end if;	
+	$proc_body$
+else
+	$proc_body$
+	raise notice 'pgpro_scheduler extension is not installed';
+	$proc_body$
 end
-$procedure$;			
+);
+end
+$q$;						
+			
