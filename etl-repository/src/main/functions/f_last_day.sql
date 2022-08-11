32173831729895f69a962c0068df302a37480af9
rsafiullin <independentoriginator@gmail.com>
2022-08-11 19:44:42 +0500

    Oracle MONTHS_BETWEEN function equivalent

---
+++ etl-repository/src/main/functions/f_last_day.sql
@@ -0,0 +1,11 @@
+create or replace function f_last_day(
+	date
+)
+returns date
+language sql
+immutable
+strict
+parallel safe
+as $function$
+select (date_trunc('month', $1) + interval '1 month' - interval '1 day')::date
+$function$;		
---
+++ etl-repository/src/main/functions/f_months_between.sql
@@ -0,0 +1,22 @@
+create or replace function f_months_between(
+	date
+	, date
+)
+returns float
+language sql
+immutable
+strict
+parallel safe
+as $function$
+select
+	(extract(year from $1) - extract(year from $2)) * 12.0
+	+ extract(month from $1) - extract(month from $2)
+	+ case
+		when extract(day from $2) = extract(day from ${mainSchemaName}.f_last_day($2))
+			and extract(day from $1) = extract(day from ${mainSchemaName}.f_last_day($1))
+		then
+			0
+		else
+			(extract(day from $1) - extract(day from $2)) / 31.0
+	end
+$function$;		
