a13fe2c7d6453981fa2e30b892639e92ba2f3019
rsafiullin <independentoriginator@gmail.com>
2022-12-04 14:11:40 +0500

    Fix quoting

--- etl-repository/src/main/views/v_scheduled_task.sql
+++ etl-repository/src/main/views/v_scheduled_task.sql
@@ -38,7 +38,7 @@ select
 	, t.task_session_user
 	, target_task.run_as as target_task_session_user
 	, format(
-		'call ${mainSchemaName}.p_cancel_pgpro_scheduler_subjobs(i_scheduled_task_name => %s)'
+		$$call ${mainSchemaName}.p_cancel_pgpro_scheduler_subjobs(i_scheduled_task_name => '%s')$$
 		, t.scheduled_task_name
 	) as on_err_cmd
 	, target_task.onrollback as target_on_err_cmd
