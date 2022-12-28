57bf51f0a028f8cd323908ebd349428e2d6026fd
rsafiullin <independentoriginator@gmail.com>
2022-12-28 22:05:36 +0500

    Target types conformation

--- etl-repository/src/main/functions/f_xsd_entity_attr_column_type.sql
+++ etl-repository/src/main/functions/f_xsd_entity_attr_column_type.sql
@@ -12,7 +12,7 @@ select
 		else
 			case i_entity_attr.type
 				when 'xs:string' then 
-					case when i_entity_attr.max_length > 0 then 'varchar(' || i_entity_attr.max_length::text || ')' else 'text' end
+					case when i_entity_attr.max_length > 0 then 'character varying(' || i_entity_attr.max_length::text || ')' else 'text' end
 				when 'xs:decimal' then 'numeric' 
 					|| case when i_entity_attr.total_digits > 0 then '(' || i_entity_attr.total_digits::text 
 						|| case when i_entity_attr.fraction_digits is not null then ',' || i_entity_attr.fraction_digits::text else '' end || ')' else '' end  
@@ -21,10 +21,10 @@ select
 				when 'xs:long' then 'bigint'
 				when 'xs:short' then 'smallint'
 				when 'xs:float' then 'real'
-				when 'xs:double' then 'float8'
+				when 'xs:double' then 'double precision'
 				when 'xs:boolean' then 'boolean'
 				when 'xs:date' then 'date'
-				when 'xs:dateTime' then 'timestamp'
+				when 'xs:dateTime' then 'timestamp without time zone'
 				when 'xs:time' then 'time'
 				else 'text'
 			end	
