926a5c349143863d51bba909a047d02360317310
rsafiullin <independentoriginator@gmail.com>
2023-03-22 11:11:29 +0500

    Idiocy on the part of the end user

--- etl-repository/src/main/views/v_pgpro_scheduler_job_log.sql
+++ etl-repository/src/main/views/v_pgpro_scheduler_job_log.sql
@@ -1,5 +1,17 @@
 create or replace view v_pgpro_scheduler_job_log
 as
+with 
+	view_obj as (
+		select
+			pg_get_userbyid(v.relowner) AS view_owner
+		from
+			pg_catalog.pg_class v
+		join pg_catalog.pg_namespace s 
+			on s.oid = v.relnamespace
+			and s.nspname = '${mainSchemaName}'
+		where 
+			v.relname = 'v_pgpro_scheduler_job_log'
+	)
 select 
 	t.job_id
 	, t.job_name
@@ -7,12 +19,13 @@ select
 	, t.started
 	, t.finished
 	, t.run_duration		
-	, t.status
-	, t.message
+	, case when view_obj.view_owner = current_user then t.status else 'done' end as status
+	, case when view_obj.view_owner = current_user then t.message else null end as message
 from 
 	${mainSchemaName}.f_pgpro_scheduler_job_log(
 		i_job_id => null
 		, i_job_name => null
 	) t
+	, view_obj
 ;
 
