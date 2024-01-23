792671b07f281b1c314e8453bd30a14caf3d9ab0
rsafiullin <independentoriginator@gmail.com>
2024-01-23 18:02:09 +0500

    Extended version of the function

--- etl-repository/src/main/functions/f_generate_pgpro_scheduler_subjobs.sql
+++ etl-repository/src/main/functions/f_generate_pgpro_scheduler_subjobs.sql
@@ -8,15 +8,19 @@ drop function if exists f_generate_pgpro_scheduler_subjobs(
 
 execute format($func$
 create or replace function f_generate_pgpro_scheduler_subjobs(
-	i_scheduled_task_stage_id ${mainSchemaName}.scheduled_task_stage.id%%type 
-	, i_iteration_number ${stagingSchemaName}.scheduled_task_subjob.iteration_number%%type
-	, i_commands text[]
+	i_commands text[]
+	, i_depends_on ${type.id}[] = null
+	, i_scheduled_task_stage_id ${mainSchemaName}.scheduled_task_stage.id%%type = null 
+	, i_iteration_number ${stagingSchemaName}.scheduled_task_subjob.iteration_number%%type = 0
+	, i_depends_on_previous_iteration_subjobs boolean = true
 )
-returns void
+returns ${type.id}[]
 language plpgsql
 as $function$
 declare 
+	l_subjobs ${type.id}[];
 	l_subjob_id ${type.id};
+	l_iteration_number ${stagingSchemaName}.scheduled_task_subjob.iteration_number%%type := coalesce(i_iteration_number, 0); 
 	l_prev_iteration_subjobs ${type.id}[];
 	l_command text;
 begin
@@ -25,9 +29,11 @@ end
 $function$;			
 
 comment on function f_generate_pgpro_scheduler_subjobs(
-	${mainSchemaName}.scheduled_task_stage.id%%type 
+	text[]
+	, ${type.id}[]
+	, ${mainSchemaName}.scheduled_task_stage.id%%type 
 	, ${stagingSchemaName}.scheduled_task_subjob.iteration_number%%type
-	, text[]
+	, boolean
 ) is 'Генерация подзадач планового задания pgpro_scheduler';	
 $func$
 , case 
@@ -35,63 +41,74 @@ $func$
 		i_scheduler_type_name => 'pgpro_scheduler'
 	) then 
 	$func_body$
-	if i_iteration_number < 0 then
-		raise exception 'Invalid iteration number specified: %', i_iteration_number;
+	if l_iteration_number < 0 then
+		raise exception 'Invalid iteration number specified: %', l_iteration_number;
 	end if;
 
-	select 
-		array_agg(subjob.id)
-	into
-		l_prev_iteration_subjobs
-	from (	
+	if i_scheduled_task_stage_id is not null
+		and i_depends_on_previous_iteration_subjobs 
+	then
 		select 
-			subjob.id
-		from 
-			${mainSchemaName}.scheduled_task_stage task_stage
-		join ${stagingSchemaName}.scheduled_task_subjob subjob
-			on subjob.scheduled_task_stage_id = task_stage.id
-			and i_iteration_number > 0
-			and subjob.iteration_number = i_iteration_number - 1 
-		where
-			task_stage.id = i_scheduled_task_stage_id
-		union all
-		select 
-			subjob.id
-		from 
-			${mainSchemaName}.scheduled_task_stage task_stage
-		join ${mainSchemaName}.scheduled_task_stage prev_task_stage
-			on prev_task_stage.scheduled_task_id = task_stage.scheduled_task_id
-			and prev_task_stage.ordinal_position < task_stage.ordinal_position
-			and prev_task_stage.is_disabled = false
-		join ${stagingSchemaName}.scheduled_task_subjob subjob
-			on subjob.scheduled_task_stage_id = prev_task_stage.id 
-		where
-			task_stage.id = i_scheduled_task_stage_id
-	) subjob
-	;
-	
+			array_agg(subjob.id)
+		into
+			l_prev_iteration_subjobs
+		from (	
+			select 
+				subjob.id
+			from 
+				${mainSchemaName}.scheduled_task_stage task_stage
+			join ${stagingSchemaName}.scheduled_task_subjob subjob
+				on subjob.scheduled_task_stage_id = task_stage.id
+				and l_iteration_number > 0
+				and subjob.iteration_number = l_iteration_number - 1 
+			where
+				task_stage.id = i_scheduled_task_stage_id
+			union all
+			select 
+				subjob.id
+			from 
+				${mainSchemaName}.scheduled_task_stage task_stage
+			join ${mainSchemaName}.scheduled_task_stage prev_task_stage
+				on prev_task_stage.scheduled_task_id = task_stage.scheduled_task_id
+				and prev_task_stage.ordinal_position < task_stage.ordinal_position
+				and prev_task_stage.is_disabled = false
+			join ${stagingSchemaName}.scheduled_task_subjob subjob
+				on subjob.scheduled_task_stage_id = prev_task_stage.id 
+			where
+				task_stage.id = i_scheduled_task_stage_id
+		) subjob
+		;
+	end if;
+
 	foreach l_command in array i_commands loop
 		l_subjob_id := 
 			schedule.submit_job(
 				query => l_command
-				, depends_on => l_prev_iteration_subjobs
+				, depends_on => array_cat(i_depends_on, l_prev_iteration_subjobs)
 			);
 		
 		if l_subjob_id is null then
 			raise exception 'Cannot create one-time pgpro_scheduler job';
 		end if;
-		
-		insert into ${stagingSchemaName}.scheduled_task_subjob(
-			id
-			, scheduled_task_stage_id
-			, iteration_number
-		)
-		values(
-			l_subjob_id
-			, i_scheduled_task_stage_id
-			, i_iteration_number
-		);
+	
+		l_subjobs := array_append(l_subjobs, l_subjob_id);
+	
+		if i_scheduled_task_stage_id is not null then
+			insert into ${stagingSchemaName}.scheduled_task_subjob(
+				id
+				, scheduled_task_stage_id
+				, iteration_number
+			)
+			values(
+				l_subjob_id
+				, i_scheduled_task_stage_id
+				, l_iteration_number
+			);
+		end if;
 	end loop;	
+
+	return 
+		l_subjobs;
 	$func_body$
 else
 	$func_body$
