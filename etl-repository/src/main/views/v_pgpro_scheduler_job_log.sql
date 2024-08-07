7e40d271af3c327c02c6cddf5bf5947c7a8c6bb8
rsafiullin <independentoriginator@gmail.com>
2024-08-07 16:18:02 +0500

    Excluded warning messages

--- etl-repository/src/main/views/v_pgpro_scheduler_job_log.sql
+++ etl-repository/src/main/views/v_pgpro_scheduler_job_log.sql
@@ -16,6 +16,7 @@ from
 	) t
 where 
 	t.job_name like '${mainSchemaName}%'
+	and (t.message is null or t.message not ilike 'max instances limit reached%')
 ;
 
 comment on view v_pgpro_scheduler_job_log is 'Журнал исполнения плановых заданий pgpro_scheduler';
