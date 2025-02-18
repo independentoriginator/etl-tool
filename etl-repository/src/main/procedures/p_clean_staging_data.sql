d1cb740a032a3a0e9310b9e5735d88b2a8004e26
rsafiullin <independentoriginator@gmail.com>
2025-02-18 17:32:29 +0500

    Ability to regularly check and automatically repair for the unlogged tables in the staging schema

--- etl-repository/src/main/procedures/p_clean_staging_data.sql
+++ etl-repository/src/main/procedures/p_clean_staging_data.sql
@@ -1,11 +1,19 @@
-create or replace procedure p_clean_staging_data(
-	i_data_package_expiration_age_in_months integer = 3
-)
+create or replace procedure 
+	p_clean_staging_data(
+		i_data_package_expiration_age_in_months integer = 3
+	)
 language plpgsql
 as $procedure$
 declare 
-	l_data_package_id ${stagingSchemaName}.data_package.id%type;
+	l_data_package_id ${stagingSchemaName}.data_package.id%type
+	;
 begin
+	call 
+		${stagingSchemaName}.p_truncate_corrupted_unlogged_tables(
+			i_schema_name => '${stagingSchemaName}'
+		)
+	;
+
 	for l_data_package_id in (
 		select 
 			id
@@ -15,12 +23,16 @@ begin
 			${mainSchemaName}.f_months_between(
 				current_date
 				, create_date::date
-			)::integer >= i_data_package_expiration_age_in_months
+			)::integer 
+			>= i_data_package_expiration_age_in_months
 	) loop
-		call ${stagingSchemaName}.p_delete_data_package(
-			i_data_package_id => l_data_package_id
-		);
-	end loop;
+		call 
+			${stagingSchemaName}.p_delete_data_package(
+				i_data_package_id => l_data_package_id
+			)
+		;
+	end loop
+	;
 
 	delete from  
 		${stagingSchemaName}.materialized_view_refresh_duration
@@ -28,11 +40,15 @@ begin
 		${mainSchemaName}.f_months_between(
 			current_date
 			, start_time::date
-		)::integer >= i_data_package_expiration_age_in_months
+		)::integer 
+		>= i_data_package_expiration_age_in_months
 	;
 end
-$procedure$;			
+$procedure$
+;			
 
-comment on procedure p_clean_staging_data(
-	integer
-) is 'Очистка промежуточных данных';
+comment on procedure 
+	p_clean_staging_data(
+		integer
+	) is 'Очистка промежуточных данных'
+;
